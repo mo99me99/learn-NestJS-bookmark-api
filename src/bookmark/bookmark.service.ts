214b1c32d6a882b199ba35bac1c4052f66f1829a
mo99me99 <m.m.hosseini1099@gmail.com>
2024-09-08 21:17:08 +0330

    auth, users, bookmarks : e2e test

--- src/bookmark/bookmark.service.ts
+++ src/bookmark/bookmark.service.ts
@@ -9,7 +9,7 @@ export class BookmarkService {
   constructor(private prisma: PrismaService) {}
 
   getBookmarks(userId: number) {
-    const bookmarks = this.prisma.bookmark.findMany({
+    return this.prisma.bookmark.findMany({
       where: {
         userId: userId,
       },
@@ -36,7 +36,7 @@ export class BookmarkService {
     bookmarkId: number,
     dto: EditBookmarkDto,
   ) {
-    const bookmark: Bookmark = await this.prisma.bookmark.findUnique({
+    const bookmark:Bookmark = await this.prisma.bookmark.findUnique({
       where: {
         id: bookmarkId,
       },
@@ -54,10 +54,25 @@ export class BookmarkService {
     });
   }
 
-  deleteBookmarkById(userId: number, bookmarkId: number) {
-    return this.prisma.bookmark.delete({
+  async deleteBookmarkById(
+    userId: number,
+    bookmarkId: number,
+  ) {
+    const bookmark =
+      await this.prisma.bookmark.findUnique({
+        where: {
+          id: bookmarkId,
+        },
+      });
+
+    // check if user owns the bookmark
+    if (!bookmark || bookmark.userId !== userId)
+      throw new ForbiddenException(
+        'Access to resources denied',
+      );
+
+    await this.prisma.bookmark.delete({
       where: {
-        userId: userId,
         id: bookmarkId,
       },
     });
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,15 +1,31 @@
 import { Test } from '@nestjs/testing';
 import { AppModule } from '../src/app.module';
-import { Body, INestApplication, ValidationPipe } from '@nestjs/common';
+import {
+  Body,
+  ForbiddenException,
+  INestApplication,
+  ValidationPipe,
+} from '@nestjs/common';
 import { PrismaService } from '../src/prisma/prisma.service';
 import * as pactum from 'pactum';
+import * as argon from 'argon2';
 import { AuthDto } from 'src/auth/dto';
 import { EditUserDto } from 'src/user/dto';
 import { createBookmarkDto } from 'src/bookmark/dto';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
 
 describe('App e2e', () => {
+  let config: ConfigService;
   let app: INestApplication;
   let prisma: PrismaService;
+  let jwt: JwtService;
+  // CONSTANTS
+  const USER_1_PASSWORD = 'USER_1_PASSWORD';
+  const USER_2_PASSWORD = 'USER_2_PASSWORD';
+  const USER_1_EMAIL = 'user1@test.com';
+  const USER_2_EMAIL = 'user2@test.com';
+
   beforeAll(async () => {
     const moduleRef = await Test.createTestingModule({
       imports: [AppModule],
@@ -23,14 +39,43 @@ describe('App e2e', () => {
     await app.init();
     await app.listen(3333);
     prisma = app.get(PrismaService);
-    await prisma.cleanDb();
+    config = app.get(ConfigService);
+    jwt = app.get(JwtService);
     pactum.request.setBaseUrl('http://localhost:3333');
   });
   afterAll(() => {
     app.close();
   });
+  beforeEach(async () => {
+    await prisma.cleanDb();
+    // prepopulate db USERS
+    let hash: string;
+    // signed up user1
+    hash = await argon.hash(USER_1_PASSWORD);
+    const user1 = await prisma.user.create({
+      data: {
+        email: USER_1_EMAIL,
+        hash: hash,
+      },
+    });
+    // signed up user2
+    hash = await argon.hash(USER_2_PASSWORD);
+    const user2 = await prisma.user.create({
+      data: {
+        email: USER_2_EMAIL,
+        hash: hash,
+      },
+    });
 
-  // it.todo('should pass');
+    // prepopulate db BOOKMARKS : user1
+    await prisma.bookmark.create({
+      data: { link: 'user1link.com', title: 'user1link', userId: user1.id },
+    });
+    // prepopulate db BOOKMARKS :user2
+    await prisma.bookmark.create({
+      data: { link: 'user2link.com', title: 'user2link', userId: user2.id },
+    });
+  });
 
   describe('Auth', () => {
     const dto: AuthDto = {
@@ -38,6 +83,13 @@ describe('App e2e', () => {
       password: 'pass',
     };
     describe('Signup', () => {
+      it('should throw if no body', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody({})
+          .expectStatus(400);
+      });
       it('should throw if email empty', () => {
         return pactum
           .spec()
@@ -45,13 +97,6 @@ describe('App e2e', () => {
           .withBody({
             password: 'pass',
           })
-          .expectStatus(400);
-      });
-      it('should throw if no body', () => {
-        return pactum
-          .spec()
-          .post('/auth/signup')
-          .withBody({})
           .expectStatus(400);
       });
       it('should throw if password empty', () => {
@@ -63,14 +108,21 @@ describe('App e2e', () => {
           })
           .expectStatus(400);
       });
+      it('shoud throw if email inUse', () => {
+        return pactum
+          .spec()
+          .post('/auth/signup')
+          .withBody({ email: USER_1_EMAIL, password: 'pass' })
+          .expectStatus(403);
+      });
 
       it('should signup', () => {
         return pactum
           .spec()
           .post('/auth/signup')
           .withBody(dto)
-          .expectStatus(201);
-        // .inspect();
+          .expectStatus(201)
+          .expectBodyContains('access_token');
       });
     });
 
@@ -82,7 +134,6 @@ describe('App e2e', () => {
           .withBody({})
           .expectStatus(400);
       });
-
       it('should throw if no email', () => {
         return pactum
           .spec()
@@ -90,7 +141,6 @@ describe('App e2e', () => {
           .withBody({ password: 'pass' })
           .expectStatus(400);
       });
-
       it('should throw if no password', () => {
         return pactum
           .spec()
@@ -98,49 +148,71 @@ describe('App e2e', () => {
           .withBody({ email: 'test@fake.com' })
           .expectStatus(400);
       });
-
+      it('should throw if no user with email', () => {
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .withBody({ email: 'noUserWithThisEmail@test.com', password: 'pass' })
+          .expectStatus(403);
+      });
+      it('shoud throw if password is wrong', () => {
+        return pactum
+          .spec()
+          .post('/auth/signin')
+          .withBody({ email: USER_1_EMAIL, password: 'wrongpassword' })
+          .expectStatus(403);
+      });
       it('should signin', () => {
         return pactum
           .spec()
           .post('/auth/signin')
-          .withBody(dto)
-          .expectStatus(200)
-          .stores('userAt', 'access_token');
+          .withBody({ email: USER_1_EMAIL, password: USER_1_PASSWORD })
+          .expectStatus(200);
       });
     });
   });
 
   describe('User', () => {
     describe('Get me', () => {
-      it('should get current user', () => {
+      it('should throw Unauthorized (no auth headers)', () => {
+        return pactum.spec().get('/users/me').expectStatus(401);
+      });
+      it('should get current user (with auth headers) ', async () => {
+        const { access_token } = await get_token(USER_1_EMAIL, USER_1_PASSWORD);
         return pactum
           .spec()
           .get('/users/me')
           .withHeaders({
-            Authorization: 'Bearer $S{userAt}',
+            Authorization: 'Bearer ' + access_token,
           })
           .expectStatus(200);
-        // .inspect();
       });
     });
 
     describe('Edit user', () => {
-      it('should edit current user', () => {
-        const dto: EditUserDto = {
-          firstName: 'mohammad',
-          lastName: 'Hosseini',
-        };
+      const dto: EditUserDto = {
+        firstName: 'newFN',
+        lastName: 'newLN',
+      };
+      it('should throw Unauthorized (no auth headers)', () => {
+        return pactum
+          .spec()
+          .patch('/users/edit')
+          .withBody(dto)
+          .expectStatus(401);
+      });
+      it('should edit current user', async () => {
+        const { access_token } = await get_token(USER_1_EMAIL, USER_1_PASSWORD);
         return pactum
           .spec()
           .patch('/users/edit')
           .withHeaders({
-            Authorization: 'Bearer $S{userAt}',
+            Authorization: 'Bearer ' + access_token,
           })
           .withBody(dto)
           .expectStatus(200)
           .expectBodyContains(dto.firstName)
           .expectBodyContains(dto.lastName);
-        // .inspect();
       });
     });
   });
@@ -152,46 +224,122 @@ describe('App e2e', () => {
       description: 'des',
     };
     describe('Create bookmarks', () => {
-      it('should create bookmark', () => {
+      it('should Throw Unauthorized if no auth header', () => {
+        return pactum.spec().post('/bookmarks/').expectStatus(401);
+      });
+      it('should Throw bad request if no link', async () => {
+        const { access_token } = await get_token(USER_1_EMAIL, USER_1_PASSWORD);
+        return pactum
+          .spec()
+          .post('/bookmarks/')
+          .withHeaders({
+            Authorization: 'Bearer ' + access_token,
+          })
+          .withBody({ title: 't', description: 'd' })
+          .expectStatus(400);
+      });
+      it('should Throw bad request if no title', async () => {
+        const { access_token } = await get_token(USER_1_EMAIL, USER_1_PASSWORD);
+
+        return pactum
+          .spec()
+          .post('/bookmarks/')
+          .withHeaders({
+            Authorization: 'Bearer ' + access_token,
+          })
+          .withBody({ link: 'l', description: 'd' })
+          .expectStatus(400);
+      });
+      it('should create bookmark with no description', async () => {
+        const { access_token } = await get_token(USER_1_EMAIL, USER_1_PASSWORD);
         return pactum
           .spec()
           .post('/bookmarks')
           .withHeaders({
-            Authorization: 'Bearer $S{userAt}',
+            Authorization: 'Bearer ' + access_token,
+          })
+          .withBody({ title: 't', link: 'l' })
+          .expectStatus(201)
+          .stores('bookmarkId', 'id');
+      });
+      it('should create bookmark ', async () => {
+        const { access_token } = await get_token(USER_1_EMAIL, USER_1_PASSWORD);
+        return pactum
+          .spec()
+          .post('/bookmarks')
+          .withHeaders({
+            Authorization: 'Bearer ' + access_token,
           })
           .withBody(dto)
           .expectStatus(201)
           .stores('bookmarkId', 'id');
-        // .inspect();
       });
     });
     describe('Get bookmarks', () => {
-      it('should get all bookmarks of a user', () => {
+      it('should get all bookmarks of a user', async () => {
+        const { access_token } = await get_token(USER_1_EMAIL, USER_1_PASSWORD);
         return pactum
           .spec()
           .get('/bookmarks')
-          .withHeaders({ Authorization: 'Bearer $S{userAt}' })
+          .withHeaders({ Authorization: 'Bearer ' + access_token })
+          .expectJsonLength(1)
           .expectStatus(200);
-        // .inspect();
       });
     });
     describe('Get bookmark by ID', () => {
-      it('should get bookmark by id (for specefic user)', () => {
+      it('should get bookmark by id (for specefic user)', async () => {
+        const { access_token } = await get_token(USER_1_EMAIL, USER_1_PASSWORD);
+        const { id: userId } = await prisma.user.findUnique({
+          where: { email: USER_1_EMAIL },
+        });
+
+        const bookmark = await prisma.bookmark.findFirst({
+          where: { userId: userId },
+        });
         return pactum
           .spec()
-          .get('/bookmarks/$S{bookmarkId}')
-          .withHeaders({ Authorization: 'Bearer $S{userAt}' })
+          .get('/bookmarks/' + bookmark.id)
+          .withHeaders({ Authorization: 'Bearer ' + access_token })
           .expectStatus(200)
-          .expectBodyContains('title')
-          .inspect();
+          .expectBodyContains('title');
+      });
+      it('should Throw Unauthorized (no auth headers)', async () => {
+        const { id: userId } = await prisma.user.findUnique({
+          where: { email: USER_1_EMAIL },
+        });
+
+        const bookmark = await prisma.bookmark.findFirst({
+          where: { userId: userId },
+        });
+        return pactum
+          .spec()
+          .get('/bookmarks/' + bookmark.id)
+          .expectStatus(401);
       });
     });
     describe('Edit bookmark', () => {
-      it('should edit bookmark', () => {
+      it('should Throw forbidden (not edit another user bookmark)',async()=>{
+        const { access_token } = await get_token(USER_1_EMAIL, USER_1_PASSWORD);
+        const { id: userId } = await prisma.user.findUnique({
+          where: { email: USER_2_EMAIL },
+        });
+        const bookmark =  await prisma.bookmark.findFirst({
+          where: { userId: userId },
+        });
+        return pactum.spec().patch('/bookmarks/'+bookmark.id).withHeaders({Authorization:'Bearer '+access_token}).expectStatus(403)
+      })
+      it('should edit bookmark', async () => {
+        const { access_token } = await get_token(USER_1_EMAIL, USER_1_PASSWORD);
+        const { id: userId } = await prisma.user.findUnique({
+          where: { email: USER_1_EMAIL },
+        });
+        const bookmark = await prisma.bookmark.findFirst({
+          where: { userId: userId },
+        });
         return pactum
           .spec()
-          .patch('/bookmarks/$S{bookmarkId}')
-          .withHeaders({ Authorization: 'Bearer $S{userAt}' })
+          .patch('/bookmarks/' + bookmark.id)
+          .withHeaders({ Authorization: 'Bearer ' + access_token })
           .withBody({
             title: 'new title',
             link: 'newlink.com',
@@ -199,26 +347,81 @@ describe('App e2e', () => {
           .expectStatus(200)
           .expectBodyContains('title')
           .expectBodyContains('link');
-        // .inspect();
       });
     });
     describe('Delete bookmark by ID ', () => {
-      it('should delete a bookmark by id ', () => {
+      it('should not deleted bookmark (no auth headers)', async () => {
+        const { access_token } = await get_token(USER_1_EMAIL, USER_1_PASSWORD);
+        const { id: userId } = await prisma.user.findUnique({
+          where: { email: USER_1_EMAIL },
+        });
+        const bookmark = await prisma.bookmark.findFirst({
+          where: { userId: userId },
+        });
         return pactum
           .spec()
-          .delete('/bookmarks/$S{bookmarkId}')
-          .withHeaders({ Authorization: 'Bearer $S{userAt}' })
-          .expectStatus(204)
-          // .inspect();
+          .delete('/bookmarks/' + bookmark.id)
+          .expectStatus(401);
       });
-      // it('should not get deleted bookmark', () => {
-      //   return pactum
-      //     .spec()
-      //     .get('/bookmarks/$S{bookmarkId}')
-      //     .withHeaders({ Authorization: 'Bearer $S{userAt}' })
-      //     .expectStatus(404)
-      //     .inspect();
-      // });
+      it('should not deleted bookmark of another user', async () => {
+        const { access_token } = await get_token(USER_1_EMAIL, USER_1_PASSWORD);
+        const { id: userId } = await prisma.user.findUnique({
+          where: { email: USER_2_EMAIL },
+        });
+        const bookmark = await prisma.bookmark.findFirst({
+          where: { userId: userId },
+        });
+        return pactum
+          .spec()
+          .delete('/bookmarks/' + bookmark.id)
+          .withHeaders({ Authorization: 'Bearer ' + access_token })
+          .expectStatus(403);
+      });
+      it('should delete a bookmark by id ', async () => {
+        const { access_token } = await get_token(USER_1_EMAIL, USER_1_PASSWORD);
+        const { id: userId } = await prisma.user.findUnique({
+          where: { email: USER_1_EMAIL },
+        });
+        const bookmark = await prisma.bookmark.findFirst({
+          where: { userId: userId },
+        });
+        return pactum
+          .spec()
+          .delete('/bookmarks/' + bookmark.id)
+          .withHeaders({ Authorization: 'Bearer ' + access_token })
+          .expectStatus(204);
+      });
     });
   });
+
+  const get_token = async (email: string, password: string) => {
+    // find the user with given email
+    const user = await prisma.user.findFirst({
+      where: { email: email },
+    });
+    // if user does not exist throw exception
+    if (!user) {
+      throw new ForbiddenException('Credentials incorrect.');
+    }
+    // verify given pass
+    const verify = await argon.verify(user.hash, password);
+    // if password is not correct throw exception
+    if (!verify) {
+      throw new ForbiddenException('Credentials incorrect.');
+    }
+    return signToken(user.id, user.email);
+  };
+
+  const signToken = async (
+    userId: number,
+    email: string,
+  ): Promise<{ access_token: string }> => {
+    const payload = { sub: userId, email };
+    const secret = config.get('JWT_SECRET');
+    const token = await jwt.signAsync(payload, {
+      expiresIn: '1m',
+      secret: secret,
+    });
+    return { access_token: token };
+  };
 });

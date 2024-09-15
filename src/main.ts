56ed739fd36994ed3903214dac109de720937768
mo99me99 <m.m.hosseini1099@gmail.com>
2024-09-15 14:54:02 +0330

    using otel + jaeger UI

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
+import { otelSDK } from './tracing';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
+  await otelSDK.start();
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
